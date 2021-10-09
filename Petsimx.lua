60355de5d4ae4fa6f94d69eab8b85da17064a44e
9LuckyHub9 <92220452+9LuckyHub9@users.noreply.github.com>
2021-10-09 18:38:47 +0300

    Update Petsimx.lua

--- Petsimx.lua
+++ Petsimx.lua
@@ -1,5 +1,5 @@
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("Pet Sim X Hub", "Synapse")
+    local Window = Library.CreateLib("Pet Sim X Hub", "BloodTheme")
 
 -----------------------Egg---------------------
 
